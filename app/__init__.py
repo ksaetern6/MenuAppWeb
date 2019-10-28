f1d52ab9d7539f326c68f59526b6833f235392e4
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-10-28 03:12:21 -0700

    added pyrebase lib and line to create index template

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
+#from dotenv import load_dotenv
 
 app=Flask(__name__)
+#load_dotenv=("../.flaskenv")
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,24 @@
+import os
+import pyrebase
 from app import app
+from flask import render_template
+
+config = {
+    "apiKey": os.getenv('FIREBASE_API_KEY'),
+    "authDomain": "menuapp-f3764.firebaseapp.com",
+    "databaseURL": "https://menuapp-f3764.firebaseio.com",
+    "projectId": "menuapp-f3764",
+    "storageBucket": "menuapp-f3764.appspot.com",
+    "messagingSenderId": "77920760117",
+    "appId": "1:77920760117:web:67cf1a4799eb73e064dc7c",
+    "serviceAccount": "app/firebase-private-key.json",
+    "measurementId": "G-33NWQZKM7W"
+
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    return render_template('/index.html', title="index title")
